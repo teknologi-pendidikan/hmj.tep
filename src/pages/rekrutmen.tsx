7fe46a372dc170cf9580c85d1578f063cc5d8277
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2022-01-07 20:03:24 +0700

    fix: rekrutmen content and links

--- src/pages/rekrutmen.tsx
+++ src/pages/rekrutmen.tsx
@@ -1,19 +1,22 @@
+import { useRouter } from "next/router";
 import type { NextPage } from "next";
+import Image from "next/image";
 
-const rekrutmen: NextPage = () => {
+const Rekrutmen: NextPage = () => {
+  const router = useRouter();
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-50">
-        <body class="h-full">
-        ```
-      */}
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-md w-full space-y-8">
+        <div className="max-w-2xl w-full space-y-8">
           <div>
+            <Image
+              src={"/banner-atas.png"}
+              alt="banner"
+              layout="responsive"
+              className="mb-4"
+              width={300}
+              height={100}
+            />
             <h1 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
               Rekrutmen HMJ TEP UM
             </h1>
@@ -22,7 +25,7 @@ const rekrutmen: NextPage = () => {
             </p>
           </div>
           <div>
-            <h2 className="mt-6 text-center text-xl font-extrabold text-gray-900">
+            <h2 className="mt-6 text-center text-2xl font-extrabold text-gray-900">
               Selayang Pandang
             </h2>
             <p className="mt-2 text-sm text-gray-600">
@@ -34,16 +37,55 @@ const rekrutmen: NextPage = () => {
               selamat datang bagi para calon pengurus HMJ TEP UM 2022.
             </p>
           </div>
+          {/* Section Formulir */}
+          <form
+            className="mt-8 space-y-6"
+            action="https://wa.me/6285748250120"
+            method="get"
+            target="_blank"
+          >
+            <h2 className="mt-6 text-center text-2xl font-extrabold text-gray-900">
+              Unduh Formulir Pendaftaran
+            </h2>
+            <div className="shadow-sm">
+              <div>
+                <label
+                  htmlFor="email-address"
+                  className="text-md font-semibold text-center"
+                >
+                  Masukkan NIM
+                </label>
+                <input
+                  id="text"
+                  name="text"
+                  type="number"
+                  required
+                  className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-800 focus:border-blue-800 focus:z-10 sm:text-sm"
+                  placeholder="Nomor Induk Mahasiswa"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full h-12 flex items-center justify-center py-2 px-4 border border-transparent text-xl font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              >
+                <span className="flex items-center pl-3"></span>
+                Unduh Formulir
+              </button>
+            </div>
+          </form>
           <div>
-            <h2 className="mt-6 text-center text-xl font-extrabold text-gray-900">
+            <h2 className="mt-6 text-center text-2xl font-extrabold text-gray-900">
               Timeline Pendaftaran
             </h2>
             <ul className="mt-2 mx-4 text-sm list-disc text-gray-600">
               <li>
-                <b>07 - 15 Januari</b> : Pengambilan dan pengembalian formulir
+                <b>07 - 14 Januari</b> : Pengambilan dan pengembalian formulir
               </li>
               <li>
-                <b>16 - 18 Januari</b> : Tes Wawancara
+                <b>15 - 17 Januari</b> : Tes Wawancara
               </li>
               <li>
                 <b>19 Januari </b>: Pengumuman hasil open recruitment
@@ -51,7 +93,7 @@ const rekrutmen: NextPage = () => {
             </ul>
           </div>
           <div>
-            <h2 className="mt-6 text-center text-xl font-extrabold text-gray-900">
+            <h2 className="mt-6 text-center text-2xl font-extrabold text-gray-900">
               Persyaratan
             </h2>
             <ol className="mt-2 mx-4 text-sm text-gray-600 list-decimal">
@@ -72,10 +114,42 @@ const rekrutmen: NextPage = () => {
             </ol>
           </div>
           <div>
-            <h2 className="mt-6 text-center text-xl font-extrabold text-gray-900">
+            <h2 className="mt-6 text-center text-2xl font-extrabold text-gray-900">
               Penjelasan Divisi
             </h2>
-            <h3 className="mt-6 text-center text-xl font-extrabold text-gray-900">
+            <h3 className="mt-6 text-center text-lg font-extrabold text-gray-900">
+              Divisi Penalaran
+            </h3>
+            <p className="mt-2 text-sm text-gray-600">
+              Divisi Penalaran merupakan divisi yang bergerak pada pengembangan
+              sumber daya manusia (PSDM) dibidang akademik dengan ranah
+              Pendidikan,Penelitian,Pelatihan,dan pengembangan (4P). Tujuan
+              utama divisi penalaran adalah mengembangkan taraf intelektual dan
+              kompetensi kognitif dari mahasiswa TEP.
+            </p>
+            <h3 className="mt-6 text-center text-lg font-extrabold text-gray-900">
+              Divisi Bakmin
+            </h3>
+            <p className="mt-2 text-sm text-gray-600">
+              Divisi Bakat Minat (Bakmin) merupakan divisi yang berfungsi
+              sebagai wadah untuk menyalurkan bakat maupun minat yang dimiliki
+              oleh mahasiswa Teknologi Pendidikan pada bidang non akademik.
+              Dalam tugasnya, Divisi Bakmin juga bekerja sama dengan komunitas
+              yang ada di Jurusan Teknologi Pendidikan untuk mengadakan kegiatan
+              yang dapat menyalurkan bakat dan minat mahasiswa Teknologi
+              Pendidikan.
+            </p>
+            <h3 className="mt-6 text-center text-lg font-extrabold text-gray-900">
+              Divisi Humas
+            </h3>
+            <p className="mt-2 text-sm text-gray-600">
+              Divisi Hubungan Masyarakat (Humas) merupakan divisi yang
+              menjembatani dan memfasilitasi komunikasi antar komponen jurusan.
+              Peran Divisi Humas sendiri yaitu memperkenalkan jurusan Teknologi
+              Pendidikan kepada mahasiswa baru dan masyarakat umum melalui
+              berbagai macam kegiatan-kegiatan yang akan diadakan.
+            </p>
+            <h3 className="mt-6 text-center text-lg font-extrabold text-gray-900">
               Divisi Wirakesma
             </h3>
             <p className="mt-2 text-sm text-gray-600">
@@ -84,42 +158,24 @@ const rekrutmen: NextPage = () => {
               Pendidikan. Tujuan pokok dari Divisi Wirakesma adalah mendatangkan
               profit untuk HMJ TEP yang nantinya digunakan sebagai pendanaan
               program kegiatan tahunan serta memberikan pelayanan kesejahteraan
-              bagi mahasiswa TEP
+              bagi mahasiswa TEP.
+            </p>
+            <h3 className="mt-6 text-center text-xl font-extrabold text-gray-900">
+              Divisi Medkominfo
+            </h3>
+            <p className="mt-2 text-sm text-gray-600">
+              Medkominfo atau media komunikasi dan informasi merupakan divisi
+              yang bergerak pada bidang media digital dalam mengelola dan
+              menyebarkan informasi kepada mahasiswa jurusan maupun luar Jurusan
+              Teknologi Pendidikan. Tujuannya adalah menyajikan berbagai
+              informasi digital mengenai kegiatan yang ada di Jurusan Teknologi
+              Pendidikan, serta menjalin kerja sama melalui media sosial.
             </p>
           </div>
-          {/* Section Formulir */}
-          <form className="mt-8 space-y-6" action="#" method="POST">
-            <input type="hidden" name="remember" defaultValue="true" />
-            <div className="rounded-md shadow-sm -space-y-px">
-              <div>
-                <label htmlFor="email-address" className="sr-only">
-                  Masukkan NIM
-                </label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Nomor Induk Mahasiswa"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                <span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
-                Verifikasi NIM
-              </button>
-            </div>
-          </form>
         </div>
       </div>
     </>
   );
 };
 
-export default rekrutmen;
+export default Rekrutmen;
