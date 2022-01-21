1fd443481f6395b9401020e69d4142c4037c5dbd
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2022-01-21 20:05:01 +0700

    fix: SI width height fight

--- src/pages/sistem-informasi.tsx
+++ src/pages/sistem-informasi.tsx
@@ -5,7 +5,7 @@ const Calendar: NextPage = () => {
     <div className="container justify-center align-middle">
       <div
         dangerouslySetInnerHTML={{
-          __html: `"<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQonOkUA8b_6MmCyEf0WXqU1qIbuWwnFLjviUv6URBneLkCMWyd5NeKvXGqQUwIMCsxD6CS3RJoiMso/pubhtml?widget=true&amp;headers=false"></iframe>"`,
+          __html: `"<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQonOkUA8b_6MmCyEf0WXqU1qIbuWwnFLjviUv6URBneLkCMWyd5NeKvXGqQUwIMCsxD6CS3RJoiMso/pubhtml?widget=true&amp;headers=false" style="border:0px #ffffff none;" name="Sistem Informasi Utama" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="100%" width="100%" allowfullscreen></iframe><div style="position: absolute;width: 72%;bottom: 1px;left: 0;right: 0;margin-left: auto;margin-right: auto;color: #000;text-align: center;"><small style="line-height: 1.2;font-size: 0px;background: #fff;"><a href="https://included.cc/">included.cc</a></small></div><style>.boxes2{height:212px;width:316px;} #new img{max-width:none!important;background:none!important}#iframe{max-height:none!important;max-width:none!important;background:none!important}</style></div>"`,
         }}
       />
     </div>
